a07ba588ca339872f425bede12eccefbd42c267d
Jay <myride.jay@gmail.com>
2023-02-09 01:34:49 +0900

    initial commit

--- .github/ISSUE_TEMPLATE/default-template.md
+++ .github/ISSUE_TEMPLATE/default-template.md
@@ -9,13 +9,10 @@ assignees: ''
 
 ## 설명
 
-
-
 ## 할일
 
 - [ ] tatk1
 - [ ] tatk2
 - [ ] tatk3
-
 
 ## 참조
---
+++ src/main/java/com/example/mallsignup2/Mallsignup2Application.java
@@ -0,0 +1,13 @@
+package com.example.mallsignup2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mallsignup2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mallsignup2Application.class, args);
+    }
+
+}
