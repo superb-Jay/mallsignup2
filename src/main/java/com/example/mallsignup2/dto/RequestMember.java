43c7c01a860f62e4301221632334c8af3d4854e1
Jay <myride.jay@gmail.com>
2023-02-09 02:02:44 +0900

    feat: create RequestMember

---
+++ src/main/java/com/example/mallsignup2/dto/RequestMember.java
@@ -0,0 +1,36 @@
+package com.example.mallsignup2.dto;
+
+import com.example.mallsignup2.entity.Member;
+import lombok.*;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class RequestMember {
+
+    private String userID;
+    private String password;
+    private String name;
+    private String age;
+    private String phone;
+    private String address;
+
+    public Member toEntity() {
+        return Member.builder()
+                .id(userID)
+                .password(password)
+                .name(name)
+                .age(Integer.parseInt(age))
+                .phone(phone)
+                .address(address)
+                .build();
+    }
+
+
+
+
+
+}
