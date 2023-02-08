a725a949b0728876821a7921be72fa1ccee4df76
Jay <myride.jay@gmail.com>
2023-02-09 02:11:17 +0900

    feat: create MemberController

---
+++ src/main/java/com/example/mallsignup2/controller/MemberController.java
@@ -0,0 +1,19 @@
+package com.example.mallsignup2.controller;
+
+
+import com.example.mallsignup2.dto.RequestMember;
+import com.example.mallsignup2.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    public String insertMember(RequestMember requestMember) {
+        return memberService.insertMember(requestMember);
+
+    }
+}
