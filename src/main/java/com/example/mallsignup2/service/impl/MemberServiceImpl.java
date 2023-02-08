45e8f65f0131011af80a24fe230743638cf01e5c
Jay <myride.jay@gmail.com>
2023-02-09 02:09:05 +0900

    feat: create MemberServiceImpl

---
+++ src/main/java/com/example/mallsignup2/service/impl/MemberServiceImpl.java
@@ -0,0 +1,15 @@
+package com.example.mallsignup2.service.impl;
+
+import com.example.mallsignup2.dto.RequestMember;
+import com.example.mallsignup2.service.MemberService;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class MemberServiceImpl implements MemberService {
+
+    @Override
+    public String insertMember(RequestMember requestMember) {
+        return null;
+    }
+}
