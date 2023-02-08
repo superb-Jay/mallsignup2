cfb3ff3458c82db5388d03e1b7a2dea6cbb49c9e
Jay <myride.jay@gmail.com>
2023-02-09 02:14:20 +0900

    fix: PostMapping add

--- src/main/java/com/example/mallsignup2/controller/MemberController.java
+++ src/main/java/com/example/mallsignup2/controller/MemberController.java
@@ -4,6 +4,7 @@ package com.example.mallsignup2.controller;
 import com.example.mallsignup2.dto.RequestMember;
 import com.example.mallsignup2.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,6 +13,8 @@ public class MemberController {
 
     private final MemberService memberService;
 
+
+    @PostMapping("/signup")
     public String insertMember(RequestMember requestMember) {
         return memberService.insertMember(requestMember);
 
