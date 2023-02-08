2f0476d2784187d9a49c57b6398982de63e98c07
Jay <myride.jay@gmail.com>
2023-02-09 02:08:57 +0900

    feat: create MemberService

---
+++ src/main/java/com/example/mallsignup2/service/MemberService.java
@@ -0,0 +1,8 @@
+package com.example.mallsignup2.service;
+
+import com.example.mallsignup2.dto.RequestMember;
+
+public interface MemberService {
+
+    String insertMember(RequestMember requestMember);
+}
