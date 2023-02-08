7642cd388fc49a23b126a91db9518c104812999e
Jay <myride.jay@gmail.com>
2023-02-09 02:04:30 +0900

    feat: create ResponseMember

---
+++ src/main/java/com/example/mallsignup2/dto/ResponseMember.java
@@ -0,0 +1,16 @@
+package com.example.mallsignup2.dto;
+
+import lombok.*;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ResponseMember {
+
+    private String code;
+    private String content;
+
+}
